f9394c9e7167a0a9c173ae46cc2104c8568aa4a1
marrow16 <martin.rowlinson16@gmail.com>
2022-02-05 12:31:55 +0000

    Add `BadRequestFlag` to `Violation` This effectively allows the caller of validation to determine whether to respond with `400 Bad Request` or `422 Unprocessable Entity`

--- violation.go
+++ violation.go
@@ -8,6 +8,19 @@ type Violation struct {
 	Path string
 	// Message is the violation message
 	Message string
+	// BadRequest is a flag indicating that the request could not be validated because
+	// the payload was not JSON.  This effectively allows the caller of validation to determine
+	// whether to respond with `400 Bad Request` or `422 Unprocessable Entity`
+	//
+	// Such violations are only added by the Validator.RequestValidate method where:
+	//
+	// * the request has an empty body
+	//
+	// * the request body does not parse (unmarshal) as JSON
+	//
+	// * the request body is JSON null (i.e. a request body containing just 'null') and the
+	// Validator.AllowNull is set to false
+	BadRequest bool
 }
 
 // NewViolation creates a new violation with the specified property, path and message
@@ -25,5 +38,14 @@ func NewEmptyViolation(msg string) *Violation {
 		Property: "",
 		Path:     "",
 		Message:  msg,
+	}
+}
+
+func NewBadRequestViolation(msg string) *Violation {
+	return &Violation{
+		Property:   "",
+		Path:       "",
+		Message:    msg,
+		BadRequest: true,
 	}
 }
